17787317773c6bf40ace990a1411052b50d76e9c
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-05-01 01:44:18 +0530

    initial project setup with clean architecture and postgreSQL config

---
+++ doctor_appointment_system_be/src/main/java/com/example/doctor_appointment_system_be/DoctorAppointmentSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctor_appointment_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorAppointmentSystemBeApplication.class, args);
+    }
+
+}
---
+++ doctor_appointment_system_be/src/test/java/com/example/doctor_appointment_system_be/DoctorAppointmentSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctor_appointment_system_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
